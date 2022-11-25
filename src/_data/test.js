cc68d792e12a947d12648300689cad2b2270f894
natistuomi <natalie.tuomi@elev.ga.ntig.se>
2022-11-25 10:12:34 +0100

    Lessons 6 & 7

---
+++ src/_data/test.js
@@ -0,0 +1,18 @@
+module.exports = {
+    workPlease(words) {
+        return words + "funka";
+    },
+    getLinkActiveState(itemUrl, pageUrl) {
+        let response = '';
+    
+        if (itemUrl === pageUrl) {
+          response = ' aria-current="page"';
+        }
+    
+        if (itemUrl.length > 1 && pageUrl.indexOf(itemUrl) === 0) {
+          response += ' data-state="active"';
+        }
+    
+        return response;
+      }
+}
