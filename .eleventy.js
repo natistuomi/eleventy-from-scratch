35fba72c0c054bfe621dcccc78a7f330a9e4ab66
natistuomi <natalie.tuomi@elev.ga.ntig.se>
2022-11-29 14:49:04 +0100

    Lesson19

--- .eleventy.js
+++ .eleventy.js
@@ -38,6 +38,8 @@ config.addCollection('people', collection => {
 
 // Set directories to pass through to the dist folder
 config.addPassthroughCopy('./src/images/');
+  // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
+  config.setUseGitIgnore(false);
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
