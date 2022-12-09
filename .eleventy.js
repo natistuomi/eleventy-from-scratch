b423dabe5bb70ad4a4340e2ee1629716284008ce
natistuomi <natalie.tuomi@elev.ga.ntig.se>
2022-12-09 09:44:03 +0100

    Lesson 31

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,11 @@ const rssPlugin = require('@11ty/eleventy-plugin-rss');
 // Filters
 const dateFilter = require('./src/filters/date-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
+// Transforms
+const htmlMinTransform = require('./src/transforms/html-min-transform.js');
 
+// Create a helpful production flag
+const isProduction = process.env.NODE_ENV === 'production';
 const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
 
 module.exports = config => {
@@ -36,8 +40,10 @@ config.addCollection('people', collection => {
   });
 });
 
-// Set directories to pass through to the dist folder
-config.addPassthroughCopy('./src/images/');
+// Only minify HTML if we are in production because it slows builds _right_ down
+if (isProduction) {
+  config.addTransform('htmlmin', htmlMinTransform);
+}
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
   config.setUseGitIgnore(false);
   return {
